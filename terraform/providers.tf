85c37c515c0f72f1cdf562b5a1a2082a7bddf1ed
michaelkemp <michael.5kemps@gmail.com>
2025-05-01 11:36:10 -0600

    providers template

---
+++ terraform/providers.tf
@@ -0,0 +1,41 @@
+locals {
+  account_id          = "{ACCOUNT_ID}"
+  account_environment = "{ACCOUNT_ENVIRONMENT}"
+  account_profile     = "{ACCOUNT_PROFILE}"
+  account_region      = "{ACCOUNT_REGION}"
+  github_repo         = "{GITHUB_REPO}"
+}
+
+terraform {
+  required_version = ">=1.11.0"
+  required_providers {
+    aws = {
+      version = ">=5.90.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "terraform-state-storage-{ACCOUNT_ID}"
+    key            = "{GITHUB_REPO}.tfstate"
+    region         = "{ACCOUNT_REGION}"
+    dynamodb_table = "terraform-state-lock-{ACCOUNT_ID}"
+    profile        = "{ACCOUNT_PROFILE}"
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = local.account_profile
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = local.account_profile
+  alias   = "us-west-2"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = local.account_profile
+  alias   = "us-east-1"
+}
